0983aac3616f82a7fd46d932994e6c411d0c81a2
Joel Macias <macias22@unlv.nevada.edu>
2016-02-05 08:30:36 -0800

    added links to lab 3, and added text editor info

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -2,14 +2,17 @@
 CS135L Week 3
 Topic: data types - basic arithmetic, variables and assignment
 
-Type			Keyword		Bytes   Typical Range  
-Boolean			bool		1	true or false 
-Character		char		1	-128 to 127
-Integer			int		4	–2,147,483,648 to 2,147,483,647
-Floating point		float		4	Generally ~7 decimals in precision 
-Double floating point	double 		8	Generally ~15  decimals in precision
+ 
+Primative Data types
+Type                    Keyword		Bytes       Typical Range
+Boolean                 bool		1           true or false
+Character               char		1           -128 to 127 or 0 to 255
+Integer                 int         4           –2,147,483,648 to 2,147,483,647
+Floating point          float		4           Generally ~7 decimals in precision
+Double floating point	double 		8           Generally ~15  decimals in precision
 
-source: https://msdn.microsoft.com/en-us/library/cc953fe1.aspx
+sources: https://msdn.microsoft.com/en-us/library/cc953fe1.aspx http://www.tutorialspoint.com/cplusplus/cpp_data_types.htm
+Data structure: http://www.cplusplus.com/doc/tutorial/structures/
 
 **/ 
 
@@ -55,6 +58,10 @@ int main(){
 	char aChar = 'a';
 	int charAnswer = aChar + 5;
 	cout<<"aChar + 5 => "<<charAnswer<<endl; 		// 97 + 5
+    
+    char anotherChar = 97;
+    charAnswer = anotherChar + 5;
+    cout<<"anotherChar + 5 => "<<charAnswer<<endl; 
 	
 	cout<<"5 / 2: => "<<5 / 2<<endl; 			// We probably dont want this result
 	cout<<"5.0 / 2 => "<<5.0 / 2<<endl;		// We probably want this result
