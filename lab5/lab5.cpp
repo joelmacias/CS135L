40ea2008a336a726bf7ecd29a1a5b7610f9aa5ae
Joel Macias <macias22@unlv.nevada.edu>
2016-02-19 09:16:33 -0800

    modifed lab 5

--- lab5/lab5.cpp
+++ lab5/lab5.cpp
@@ -1,42 +1,74 @@
 /*
- Lab 5 - Formatting output 
-
-iomanip - contains manipulators used to format output 
-
-setw(n) - Outputs n columns 
-        - Only applies to next value to be displayed
-	- Content will be right justified by default 
-		- cout << left << ...; will left justify content
-		- cout << right << ...; will right justify content
-
-setprecision(n) - Controls output of floating-point numbers 
-	cout << fixed << setprecision(2); 
-
-
-
-*/
+ Lab 5 - Formatting output
+ 
+ iomanip - contains manipulators used to format output
+ 
+ setw(n) - Outputs n columns
+ - Only applies to next value to be displayed
+	- Content will be right justified by default
+ - cout << left << ...; will left justify content
+ - cout << right << ...; will right justify content
+ 
+ setprecision(n) - Controls output of floating-point numbers
+	cout << fixed << setprecision(2);
+ */
 
 
 #include<iostream>
-#include<string>
 #include<iomanip>
-#include<cmath>
-using namespace std; 
+using namespace std;
 
 int main(){
-
-string mer = "Mercury", ven = "Venus", ear = "Earth", jup = "Jupiter", sat = "Saturn", ura = "Uranus", nep = "Neptune", mar = "Mars";
-
-int n = 17, l = 16;
-
-cout << "Planet Facts" << endl; 
-cout << fixed << setprecision(2); 
-cout << left << setw(l) << "Planet" << right << setw(n + 3) << "Distance From Sun" << endl; 
-cout << left << setw(l) << mer << right << setfill('.') << setw(n) << 6.98*pow(10.0, 7.0) << " km" << endl;
-cout << left << setw(l) << ven << right << setw(n) << 1.089*pow(10.0, 8.0) << " km" << endl; 
-cout << left << setw(l) << ear << right << setw(n) << 1.521*pow(10.0, 8.0) << " km" << endl;
-cout << left << setw(l) << mar << right << setw(n) << 2.491*pow(10.0, 8.0) << " km" << endl;
-
-return 0; 
-
+    
+    cout<< "Enter the length: ";
+    double length;
+    cin >> length;
+    
+    cout<< "Enter the width: ";
+    double width;
+    cin >> width;
+    
+    cout << "Enter the height: ";
+    double height;
+    cin >> height;
+    cout << endl;
+    
+    cout << "Rectangular Prism info" << endl;
+    // All floating point types will be displayed with 4 numbers after the decimal point
+    cout << fixed << setprecision(4);
+    
+    // Longest label is 28 characters, add 1 to allow a blank space for all lables. We can assume the largest number will be 7
+    cout << left << setw(29) << "length:" << right << setw(7) << length << " in" << endl;
+    
+    cout << left << setw(29) << "Width:" << right << setw(7) << width << " in" << endl;
+    
+    cout << left << setw(29) << "Height:" << right << setw(7) << height << " in" << endl;
+    
+    cout << left << setw(29) << "Volume of rectangular prism:" << right << setw(7) << length * width * height << " cu in" <<endl;
+    
+    cout << endl;
+    
+    cout << "Receipt" << endl;
+    
+    cout << fixed << setprecision(2);
+    
+    double callOfDuty = 59.99, mountainDew = 1.29, doritos = 1.99, tax = .0810;
+    
+    // Longest label is 32 characters, add at least 1 more to allow a blank space for all lables. We can assume the largest number will be 7
+    cout << left << setw(33) << "Call of Duty 19: Ancient Warfare" << "$" << right << setw(7) << callOfDuty << endl;
+    
+    cout << left << setw(33) << "Mountain Dew" << "$" << right << setw(7) << mountainDew << endl;
+    
+    cout << left << setw(33) << "Doritos" << "$" << right << setw(7) << doritos << endl;
+    
+    cout << setfill('.') << setw(33 + 7 + 1 ) << "" << endl;
+    
+    cout << setfill(' ');
+    
+    cout << left << setw(33) << "Total Cost" << "$" << right << setw(7) << ((mountainDew + callOfDuty + doritos) * tax) + mountainDew + callOfDuty + doritos << endl;
+    
+    
+    
+    
+    return 0;
 }
